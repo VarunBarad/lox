32fe57af493d9e2a8859bdd447e5a85f571fad82
Varun Barad <varun@varunbarad.com>
2025-10-20 00:27:52 +0530

    Add a script to generate "Expr" classes

---
+++ jlox/src/main/java/com/varunbarad/tool/GenerateAst.java
@@ -0,0 +1,71 @@
+package com.varunbarad.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output_directory>");
+            System.exit(64);
+        }
+        String outputDirectory = args[0];
+        System.out.println("Output Directory: " + outputDirectory);
+        defineAst(outputDirectory, "Expr", Arrays.asList(
+                "Binary : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal : Object value",
+                "Unary : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package com.varunbarad.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        // The AST classes
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+            writer.println();
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("\tstatic class " + className + " extends " + baseName + " {");
+
+        // Fields
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            writer.println("\t\tfinal " + field + ";");
+        }
+
+        writer.println();
+
+        // Constructor
+        writer.println("\t\t" + className + "(" + fieldList + ") {");
+        // Store parameters in fields
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("\t\t\tthis." + name + " = " + name + ";");
+        }
+        // Close constructor
+        writer.println("\t\t}");
+
+
+        writer.println("\t}");
+    }
+}
