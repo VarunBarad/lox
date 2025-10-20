b86678db88e3fd3b6acbd7116f47ccd55a2ed93e
Varun Barad <varun@varunbarad.com>
2025-10-20 15:21:48 +0530

    Add a class to print the Abstract Syntax Tree in Reverse-Polish Notation

---
+++ jlox/src/main/java/com/varunbarad/lox/RpnPrinter.java
@@ -0,0 +1,45 @@
+package com.varunbarad.lox;
+
+public class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private String notate(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(");
+        for (Expr expr : exprs) {
+            builder.append(expr.accept(this));
+            builder.append(" ");
+        }
+        builder.append(name);
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return notate(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) {
+            return "nil";
+        } else {
+            return expr.value.toString();
+        }
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return notate(expr.operator.lexeme, expr.right);
+    }
+}
