64c6014a694197a57a9bfb0bcdee35972994b534
Varun Barad <varun@varunbarad.com>
2025-11-01 11:54:24 +0530

    Add support for nested environments (to store variables)

--- jlox/src/main/java/com/varunbarad/lox/Environment.java
+++ jlox/src/main/java/com/varunbarad/lox/Environment.java
@@ -4,7 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    private final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    public Environment() {
+        this.enclosing = null;
+    }
+
+    public Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -13,6 +22,8 @@ public class Environment {
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
+        } else if (enclosing != null) {
+            return enclosing.get(name);
         } else {
             throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
         }
@@ -21,6 +32,8 @@ public class Environment {
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+        } else if (enclosing != null) {
+            enclosing.assign(name, value);
         } else {
             throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
         }
