593d283af0e6cc578b964f0ecccef1c2f805ac2c
Varun Barad <varun@varunbarad.com>
2026-01-15 00:16:07 +0530

    Add support for `if` statements.

--- jlox/src/main/java/com/varunbarad/lox/Parser.java
+++ jlox/src/main/java/com/varunbarad/lox/Parser.java
@@ -133,13 +133,29 @@ public class Parser {
     }
 
     private Stmt statement() {
-        if (match(PRINT)) {
+        if (match(IF)) {
+            return ifStatement();
+        } else if (match(PRINT)) {
             return printStatement();
         } else if (match(LEFT_BRACE)) {
             return new Stmt.Block(block());
         } else {
             return expressionStatement();
         }
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
